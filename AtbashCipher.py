6b5b902cd88ed63c90e85ead293e078bb8793dea
anjumkhlf <anjumkhlf@gmail.com>
2025-02-15 12:10:19 -0500

    added Atbash cipher

---
+++ AtbashCipher.py
@@ -0,0 +1,12 @@
+def encode(text):
+    text2 = ""
+    for letter in text:
+        if letter.isalpha():
+            if letter.isupper():
+                text2 += chr(ord(letter) * -1 + 155)
+            else:
+                text2 += chr(ord(letter) * -1 + 219)
+        else:
+            text2 += letter
+            
+    return text2
